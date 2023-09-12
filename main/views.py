65f86868837fd5876fe6924eab0e0f3362c606c9
RealPacifist <the.real.a.pacifist@gmail.com>
2023-09-12 17:00:52 +0700

    Changed the models and html a bit

--- main/views.py
+++ main/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'name': 'Muhammad Hilmy Abdul Aziz',
-        'class': 'PBP D'
+        'class': 'PBP D',
+        'description': 'A corpse that has died shamefully in battle.His corpse is haunted with vengefull spirits seeking his demise even after death',
+        'causeOfDeath':'An arrow to the knee',
+        'spiritStatus': 'Tormented Spirit'
     }
 
     return render(request, "main.html", context)
